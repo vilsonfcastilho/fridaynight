de045708a97e0c0f68ca6cfc402f9ef234b243ba
vilsonfcastilho <vilsonfcastilho@gmail.com>
2023-07-19 12:17:11 +0100

    feat: useFonts and create Loading component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
 import { Tables } from '@screens/Tables';
 
 import theme from './src/theme';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Tables />
+      {fontsLoaded ? <Tables /> : <Loading />}
     </ThemeProvider>
   );
 }
